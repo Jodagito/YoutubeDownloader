57a4bdc1f77c6f92496017c0f9cbea60bae35484
philippeitis <33013301+philippeitis@users.noreply.github.com>
2020-02-07 21:27:23 -0800

    Add support for selecting resolution and improve error messages.

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+from pytube.exceptions import PytubeError, RegexMatchError
 import requests
 import os
 
@@ -16,7 +17,7 @@ def main():
     elif download_option in ['3', '3)', 'Playlist']:
         download_playlists()
     else:
-        incorrectSelectionException()
+        handle_incorrect_selection()
 
 
 def download_playlists():
@@ -34,25 +35,50 @@ def download_playlists():
         elif option in ['2', '2)', 'Videos']:
             download_file(file_url, "video")
         else:
-            incorrectSelectionException()
+            handle_incorrect_selection()
     print("The playlist has been downloaded succesfully.\n")
 
 
 def download_file(file_url=None, file_format="song"):
     if not file_url:
-        file_url = input(f"Insert the {file_format} url ")
+        file_url = input(f"Insert the {file_format} url.")
     try:
-        print(f"Downloading {YouTube(file_url).title}")
+        vid = YouTube(file_url)
+        print(f"Downloading {vid.title}")
+
         if file_format == "song":
-            YouTube(file_url).streams.get_audio_only().download(destination_path)
+            vid.streams.get_audio_only().download(destination_path)
         elif file_format == "video":
-            YouTube(file_url).streams.get_highest_resolution().download(destination_path)
-        print(f"{YouTube(file_url).title} has been downloaded succesfully.\n")
-    except Exception as e:
-        print(f"{YouTube(file_url).title} couldn't be downloaded.\n")
+            resolution = get_resolution(vid)
+            to_download = vid.streams.get_by_resolution(resolution=resolution)
+            if to_download:
+                to_download.download(destination_path)
+            else:
+                print(f"Could not find a suitable stream for the selected resolution for {vid.title}.")
+                return
+
+        print(f"{vid.title} has been downloaded succesfully.\n")
+    except RegexMatchError as e:
+        print(f"{file_url} is not a valid youtube link. Please try again.")
+        download_file(file_format=file_format)
+    except (IOError, OSError, PytubeError) as e:
+        print(f"{file_url} couldn't be downloaded.\n{e}\n")
 
 
-def incorrectSelectionException():
+def get_resolution(vid):
+    available_resolutions = list({stream.resolution for stream in vid.fmt_streams})
+
+    if None in available_resolutions:
+        available_resolutions.remove(None)
+
+    available_resolutions.sort(key=lambda x: int(x.split("p")[0]))
+    resolution = input(f"Choose one of {', '.join(available_resolutions)} ")
+    while resolution not in available_resolutions:
+        resolution = input(f"Choose one of {', '.join(available_resolutions)} ")
+    return resolution
+
+
+def handle_incorrect_selection():
     input("\n\n\tError: Incorrect selection.")
     return main()
 
